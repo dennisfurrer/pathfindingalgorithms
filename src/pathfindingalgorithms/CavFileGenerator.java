3025c4c4784fbcb3d0460bad78739a3a03a5ef59
dennisfurrer <dennis.furrer@gmail.com>
2017-05-28 23:14:58 +0100

    Accuracy optimised as not every .cav file that was generated necessarily had a path from root to goal. Accuracy improved from 86% to 100%.

--- src/pathfindingalgorithms/CavFileGenerator.java
+++ src/pathfindingalgorithms/CavFileGenerator.java
@@ -2,7 +2,10 @@ package pathfindingalgorithms;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -13,8 +16,21 @@ public class CavFileGenerator
 		System.out.println("Please enter the absolute filepath where you would like to store the .cav file:");
 		Scanner sc = new Scanner(System.in);
 		String filepath = sc.next();
+		int noOfCavs = noOfCavs();
+		int[][] coordinates = coordinates();
+		boolean[][] connectivity = connectivity();
+		boolean pathFound = pathTest(noOfCavs, coordinates, connectivity);
+		
+		while (pathFound == false)
+		{
+			noOfCavs = noOfCavs();
+			coordinates = coordinates();
+			connectivity = connectivity();
+			pathFound = pathTest(noOfCavs, coordinates, connectivity);
+		}
+		
 		PrintWriter writer = new PrintWriter(filepath + "\\input.cav", "UTF-8");
-		String fileContents = Integer.toString(noOfCavs()) + "," + Arrays.deepToString(coordinates()) + "," + Arrays.deepToString(connectivity());
+		String fileContents = Integer.toString(noOfCavs) + "," + Arrays.deepToString(coordinates) + "," + Arrays.deepToString(connectivity);
 		String output = fileContents.replaceAll("\\[", "").replaceAll("\\]", "").replaceAll(" ", "").replaceAll("false", "0").replaceAll("true", "1");
 		writer.println(output);
 		writer.close();
@@ -44,19 +60,29 @@ public class CavFileGenerator
 	public static boolean[][] connectivity()
 	{
 		int noOfCavs = noOfCavs();
-		int[][] coordinates = coordinates();
 		boolean[][] connectivity = new boolean[noOfCavs][noOfCavs];
 		int noOfConnections;
 		
-		for (int i = 0; i < noOfCavs-1; i++)
+		for (int i = 0; i < noOfCavs-1; i++) //for each cavern
 		{
-			if (i == 0 || i == 19)
+			if (i == 0 || i == 19) //if cavern is root or goal
 			{
-				noOfConnections = ThreadLocalRandom.current().nextInt(1, 2);
+				noOfConnections = ThreadLocalRandom.current().nextInt(1, 3);
 			}
 			else
 			{
-				noOfConnections = ThreadLocalRandom.current().nextInt(1, 5);
+				if (i % 2 == 0) //if cavern no is divisible by 2 and not equal to 2, 10 or 14
+				{
+					noOfConnections = ThreadLocalRandom.current().nextInt(1, ThreadLocalRandom.current().nextInt(3,5));
+				}
+				else if (i % 5 == 0) //if cavern no is divisible by 5
+				{
+					noOfConnections = ThreadLocalRandom.current().nextInt(2, 5);
+				}
+				else
+				{
+					noOfConnections = ThreadLocalRandom.current().nextInt(ThreadLocalRandom.current().nextInt(1,3), 4);
+				}
 			}
 			
 			int[] connectionsIndice = new int[noOfConnections];
@@ -77,5 +103,116 @@ public class CavFileGenerator
 		}
 		
 		return connectivity;
+	}
+	
+	public static boolean pathTest(int noOfCavs, int[][] xyArray, boolean[][] connMatrix)
+	{
+		boolean pathFound = false;
+		
+		Cavern root = new Cavern(xyArray[0][0], xyArray[0][1], 0);
+		Cavern goal = new Cavern(xyArray[noOfCavs - 1][0], xyArray[noOfCavs - 1][1], noOfCavs-1);
+		List<Cavern> openList = new ArrayList<Cavern>();
+		List<Cavern> closedList = new ArrayList<Cavern>();
+		List<Cavern> solutionList = new ArrayList<Cavern>();
+		
+		Cavern current = root;
+		root.value = 0;
+		openList.add(root);
+		solutionList.add(root);
+		
+		while (openList.size() > 0) 
+		{
+			if (current.no != goal.no) 
+			{
+				// Neighbours
+				for (int i = 0; i < noOfCavs; i++) 
+				{
+					if (connMatrix[i][current.no]) 
+					{
+						Cavern neighbour = new Cavern(xyArray[i][0], xyArray[i][1], i);
+						if (closedList.size() == 0) 
+						{
+							current.neighbours.add(neighbour);
+							break;
+						} 
+						else 
+						{
+							for (Cavern c : closedList) 
+							{
+								if (presenceCheck(closedList, i) == false) 
+								{
+									current.neighbours.add(neighbour);
+									break;
+								}
+							}
+						}
+					}
+				}
+
+				openList.remove(current);
+				closedList.add(current);
+
+				for (Cavern neighbour : current.neighbours) 
+				{
+					neighbour.value = distance(current, neighbour);
+					double potentialValue = neighbour.value + current.value;
+
+					if (presenceCheck(openList, neighbour.no) == false && presenceCheck(closedList, neighbour.no) == false) 
+					{
+						neighbour.value = potentialValue;
+						neighbour.parent = current;
+						openList.add(neighbour);
+					}
+				}
+				
+				ArrayList<Double> cavernValues = new ArrayList<Double>();
+				
+				for (Cavern c : openList) 
+				{
+					cavernValues.add(c.value);
+					double minValue = Collections.min(cavernValues);
+					
+					if (c.value == minValue) 
+					{
+						current = c;
+						solutionList.add(current);
+					}
+				}
+			}
+			else
+			{
+				break;
+			}
+		}
+		
+		//pathFound = ((openList.size() == 0 && goal.no != current.no) ? false : true);
+		//pathFound = current.no == goal.no ? true : false;
+		
+		if (current.no == goal.no)
+		{
+			goal.value = current.value;
+			pathFound = true;
+		}
+		
+		return pathFound;
+	}
+	
+	private static double distance(Cavern c1, Cavern c2) 
+	{
+		double distance = Math.sqrt(Math.pow((c2.y - c1.y), 2) + Math.pow((c2.x - c1.x), 2));
+		return distance;
+	}
+
+	private static boolean presenceCheck(List<Cavern> caverns, int cavNo) 
+	{
+		for (Cavern c : caverns) 
+		{
+			if (c.no == cavNo) 
+			{
+				return true;
+			}
+		}
+
+		return false;
 	}
 }
